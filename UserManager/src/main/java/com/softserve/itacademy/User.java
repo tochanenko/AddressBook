30c678fcc4eb3bfb3cbd6c6ad92839e63ad96402
Vladislav <itochanenko@gmail.com>
2020-05-24 10:20:29 +0300

    Change Java version to 12. Add UserDao class

--- UserManager/src/main/java/com/softserve/itacademy/User.java
+++ UserManager/src/main/java/com/softserve/itacademy/User.java
@@ -17,8 +17,8 @@ public class User {
         this.password = password;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public String getUsername() {
