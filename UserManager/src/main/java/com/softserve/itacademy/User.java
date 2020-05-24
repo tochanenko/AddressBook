9039d7c46dec21e5a738d5599fce9c8798d17e1f
Vladislav <itochanenko@gmail.com>
2020-05-24 10:00:15 +0300

    Create User class

---
+++ UserManager/src/main/java/com/softserve/itacademy/User.java
@@ -0,0 +1,39 @@
+package com.softserve.itacademy;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    private static int counter = 1;
+
+    public User() {
+        this.id = counter++;
+    }
+
+    public User(String username, String password) {
+        this.id = counter++;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
